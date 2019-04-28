7cff2071826467f53567f3a8cccecb832351eb0e
satya surya sai pavan korubilli <saipavan5th@gmail.com>
2019-04-28 20:49:10 +0530

    Add files via upload

---
+++ Datatypes.java
@@ -0,0 +1,26 @@
+
+public class Datatypes {
+
+	public static void main(String[] args) {
+		int myInt = 81;
+		short myShort= 812;
+	    long myLong=7681;
+	    
+	    double myDouble=4.5674;
+	    char myChar='p';
+	    
+	    float myFloat=5.67f;
+	    boolean myBoolean=true;
+	    byte myByte= 1;
+	    
+	    System.out.println(myInt);
+	    System.out.println(myShort);
+	    System.out.println(myLong);
+	    System.out.println(myChar);
+	    System.out.println(myDouble);
+	    System.out.println(myBoolean);
+	    System.out.println(myFloat);
+	    System.out.println(myByte);
+	}
+
+}
