faf10fff33ceb5881dd8ce41c5d0ae37f39d807e
satya surya sai pavan korubilli <saipavan5th@gmail.com>
2019-04-28 21:09:04 +0530

    Add files via upload

---
+++ Strings.java
@@ -0,0 +1,17 @@
+
+public class Strings {
+
+	public static void main(String[] args) {
+		int number= 7;
+		String text= "hello";
+		String blank= " ";
+		String name= "pavan";
+		
+		String total=text+blank+name;
+		
+		System.out.println(total);
+		
+		System.out.println("my number is: "+number);
+	}
+
+}
