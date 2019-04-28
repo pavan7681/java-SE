c9262b27aeba3259119735fa37cba1a42c0ac216
satya surya sai pavan korubilli <saipavan5th@gmail.com>
2019-04-28 21:04:32 +0530

    Add files via upload

---
+++ Ifexample.java
@@ -0,0 +1,19 @@
+
+public class Ifexample {
+
+	public static void main(String[] args) {
+		int loop=0;
+		while(true)
+		{
+		if(loop==5)
+		{
+			break;
+		}
+		loop++;
+			System.out.println("looping " +loop);
+			System.out.println("running");
+		}
+
+	}
+
+}
