8e7b41f51257fc4cf855686999c5dbd3dc14f6f6
satya surya sai pavan korubilli <saipavan5th@gmail.com>
2019-04-28 21:09:58 +0530

    Add files via upload

---
+++ Whileloop.java
@@ -0,0 +1,13 @@
+
+public class Whileloop {
+
+	public static void main(String[] args) {
+		int data=0;
+		while(data<10)
+		{
+          System.out.println("score is "+data);
+          data =data + 1;
+	}
+
+}
+}
