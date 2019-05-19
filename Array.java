65e5b25ce18ce801f343d091f3f456a64944e72f
satya surya sai pavan korubilli <saipavan5th@gmail.com>
2019-05-19 07:51:11 +0530

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,18 @@
+
+public class Array {
+
+	public static void main(String[] args) {
+	   int[] values=new int[3];
+	   values[0]=7;
+	   values[1]=8;
+	   values[2]=9;
+	   for(int i=0;i<values.length;i++)
+	   {
+		   System.out.println(values[i]);
+	}
+	   int[] a=new int[5];
+	   a[4]=76;
+		System.out.println(a[4]);
+	}
+
+}
