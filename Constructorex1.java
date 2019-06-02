6ffd8ea43007cce49ea56182caa3b42dbd03852a
satya surya sai pavan korubilli <saipavan5th@gmail.com>
2019-06-02 06:44:30 +0530

    Add files via upload

---
+++ Constructorex1.java
@@ -0,0 +1,14 @@
+
+public class Constructorex1 {
+
+	public Constructorex1() {
+		System.out.print("yuvi is cricketer");
+		
+	}
+
+	public static void main(String[] args) {
+	Constructorex1 obj=new Constructorex1();
+
+	}
+
+}
